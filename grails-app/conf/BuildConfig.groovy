868124c518b72ce549f1ef8083310089ff0ab5e5
Stefan Wold <stefan.wold@unitedid.org>
2014-03-15 10:47:13 +0100

    Framework version bump, preparing 1.0.0-dev for development
    - Build for java 1.7
    - Use servlet v3.0 in tomcat7
    - Bumped a few plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,9 +1,9 @@
-grails.servlet.version = "2.5"
+grails.servlet.version = "3.0"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 
 
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
@@ -49,17 +49,17 @@ grails.project.dependency.resolution = {
             exclude "spock-grails-support"
         }
 
-        runtime ":hibernate:$grailsVersion"
+        runtime ":hibernate:3.6.10.2"
 
-        build ":tomcat:$grailsVersion"
+        build ":tomcat:7.0.42"
 
         runtime ":greenmail:1.3.4"
         runtime ":jquery:1.10.2"
         runtime ":jquery-ui:1.8.24"
         runtime ":jquery-validation:1.9"
-        runtime ":mongodb:1.2.0"
-        runtime ":quartz:0.4.2"
-        runtime ":recaptcha:0.6.5"
+        runtime ":mongodb:1.3.3"
+        runtime ":quartz:1.0.1"
+        runtime ":recaptcha:0.6.7"
 
         //compile ":cache:1.0.1"
     }
